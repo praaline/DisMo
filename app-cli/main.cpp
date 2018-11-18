6fcce8fce31610d3fcc4772ce940b2e0b7827cb1
George Christodoulides <george@mycontent.gr>
2018-11-18 10:27:51 +0100

    CLI

--- app-cli/main.cpp
+++ app-cli/main.cpp
@@ -1,8 +1,22 @@
+#include <QString>
+#include <QList>
 #include <QCoreApplication>
+#include <QCommandLineParser>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    QCoreApplication::setApplicationName("DisMo");
+    QCoreApplication::setApplicationVersion("2018.11.1");
+
+    QCommandLineParser parser;
+    parser.setApplicationDescription("DisMo Multi-Level Linguistic Annotator");
+    parser.addHelpOption();
+    parser.addVersionOption();
+
+    parser.addPositionalArgument("input", QCoreApplication::translate("main", "Input file to annotate."));
+
+
 
     return a.exec();
 }
--- crfpp/tagger.h
+++ crfpp/tagger.h
@@ -191,9 +191,9 @@ class TaggerImpl : public Tagger {
   };
 
   class QueueElementComp {
-   public:
-    const bool operator()(QueueElement *q1,
-                          QueueElement *q2)
+    public:
+    bool operator()(QueueElement *q1,
+                    QueueElement *q2)
     { return(q1->fx > q2->fx); }
   };
 
